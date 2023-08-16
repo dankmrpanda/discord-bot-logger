d0285d5ac942ccaf368d0e95b65d312325357f63
Raymond Jiang <raymondjiang0917@gmail.com>
2023-08-16 00:07:22 -0700

    added temp sol to embed images and embeds being deleted will fix tmr or smth

--- index.js
+++ index.js
@@ -17,25 +17,38 @@ client.on('ready', (c) => {
 client.on('messageDelete', message => {
     console.log("bot del");
     if (!message.partial){
-        const channel = client.channels.cache.get('1141244330401595423')
-        let messageAttachment = message.attachments.size > 0 ? message.attachments.array()[0].url : null
+        const channel = client.channels.cache.get('1141225224910667828')
+        //let messageAttachment = message.attachments.size > 0 ? message.attachments.array()[0].url : null
         if (channel)
         {
+            try {
             const embed = new EmbedBuilder()
                 .setTitle('msg delete')
                 .addFields(
-                    { name: 'tag', value: `${message.author.tag}` },
-                    { name: 'channel', value: `${message.channel.name}` },
+                    { name: 'user', value: `${message.author}` },
+                    { name: 'channel', value: `${message.channel}` },
                 )
                 .setDescription(message.content)
                 //.setImage(message.attachments[0].url)
-                
                 .setTimestamp();
-            channel.send({ embeds: [embed] });
+                channel.send({ embeds: [embed] });
+            }
+            catch (e)
+            {
+                const embed = new EmbedBuilder()
+                .setTitle('msg delete')
+                .addFields(
+                    { name: 'user', value: `${message.author}` },
+                    { name: 'channel', value: `${message.channel}` },
+                )
+                .setDescription("null")
+                //.setImage(message.attachments[0].url)
+                .setTimestamp();
+                channel.send({ embeds: [embed] });
+            }
+            
             
         }
-        
-
     }
 })
 client.login("MTE0MTIzODMyNjM5ODAyNTgxOQ.GFGV9m.0aqWdt6r_5w0W60mBz_pf2iTM1Hak_sY6BwlyE")
